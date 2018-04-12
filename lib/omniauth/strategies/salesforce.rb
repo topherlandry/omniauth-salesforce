3b68b9ff59c046890702bac872614807e01cd58b
Chris Landry <topher.p.landry@gmail.com>
2018-04-12 14:54:58 -0400

    Specify site from environment variables.

--- lib/omniauth/strategies/salesforce.rb
+++ lib/omniauth/strategies/salesforce.rb
@@ -9,7 +9,7 @@ module OmniAuth
       MOBILE_USER_AGENTS =  'webos|ipod|iphone|ipad|android|blackberry|mobile'
 
       option :client_options, {
-        :site          => 'https://login.salesforce.com',
+        :site          => ENV['SALESFORCE_SITE'],
         :authorize_url => '/services/oauth2/authorize',
         :token_url     => '/services/oauth2/token'
       }
